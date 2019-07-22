1d5ec5b6236cb7bdb3933fdf15490f920f250591
Alexis Panyathong <alexispanyathong@gmail.com>
2019-07-22 15:19:18 -0500

    Finished
    I have completed the steps and did the imports, but not sure if I have to add anything else...

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">About</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+        <ul>
+          <li>
+            <Link to="/Home/">Home</Link>
+          </li>
+          <li>
+          <Link to="/About/">About</Link>
+          </li>
+          <li>
+          <Link to="/Contact/">Contact</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
 };
+
 
 export default Navigation;
